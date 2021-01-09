74c107e0d1be8ba8a445507747f8bf33abe1bc40
tiger20111 <nikit-proc@mail.ru>
2021-01-09 03:55:15 +0300

    removed unused from code on python

--- data/pyhton/analyse.py
+++ data/pyhton/analyse.py
@@ -2,185 +2,6 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import plotly
 import plotly.graph_objs as go
 
-def throughputRing(df):
-  df1 = df.loc[df['numberThread'] == 6]
-  df2 = df.loc[df['numberThread'] == 10]
-  df3 = df.loc[df['numberThread'] == 15]
-  df4 = df.loc[df['numberThread'] == 20]
-
-  df1 = df1.sort_values(by='timePrecessingRing')
-  df2 = df2.sort_values(by='timePrecessingRing')
-  df3 = df3.sort_values(by='timePrecessingRing')
-  df4 = df4.sort_values(by='timePrecessingRing')
-
-  trace1 = go.Scatter(
-      x=df1.timePrecessingRing,
-      y=df1.throughputChain * 6,
-      name='Threads 6'
-  )
-  trace2 = go.Scatter(
-      x=df2.timePrecessingRing,
-      y=df2.throughputChain * 10,
-      name='Threads 10'
-  )
-  trace3 = go.Scatter(
-      x=df3.timePrecessingRing,
-      y=df3.throughputChain * 15,
-      name='Threads 15'
-  )
-  trace4 = go.Scatter(
-      x=df4.timePrecessingRing,
-      y=df4.throughputChain * 20,
-      name='Threads 20'
-  )
-
-
-  fig = go.Figure(data=[trace1, trace2, trace3, trace4])
-
-  fig.update_layout(
-      title="Throughput Ring by time processing ring",
-      yaxis_title="Number accepted messanges by one sec",
-      xaxis_title="Time work ring (milliseconds)"
-  )
-
-  iplot(fig, show_link=False)
-
-
-def latency(df, min_time, max_time):
-  dfT = df.loc[(min_time <= df['timePrecessingRing']) & (df['timePrecessingRing'] < max_time)]
-
-  df1 = dfT.loc[dfT['numberThread'] == 6]
-  df2 = dfT.loc[dfT['numberThread'] == 10]
-  df3 = dfT.loc[dfT['numberThread'] == 15]
-  df4 = dfT.loc[dfT['numberThread'] == 20]
-
-  df1 = df1.sort_values(by='timePrecessingRing')
-  df2 = df2.sort_values(by='timePrecessingRing')
-  df3 = df3.sort_values(by='timePrecessingRing')
-  df4 = df4.sort_values(by='timePrecessingRing')
-
-  trace1 = go.Scatter(
-      x=df1.timePrecessingRing,
-      y=df1.latencyChain,
-      name='Threads6'
-  )
-  trace2 = go.Scatter(
-      x=df2.timePrecessingRing,
-      y=df2.latencyChain,
-      name='Threads10'
-  )
-  trace3 = go.Scatter(
-      x=df3.timePrecessingRing,
-      y=df3.latencyChain,
-      name='Threads15'
-  )
-  trace4 = go.Scatter(
-      x=df4.timePrecessingRing,
-      y=df4.latencyChain,
-      name='Threads20'
-  )
-
-  fig = go.Figure(data=[trace1, trace2, trace3, trace4], layout={'title':'Latency Chain by time processing ring'})
-
-  fig.update_layout(
-        title="Latency Chain by time processing ring",
-        yaxis_title="Time to deliver message (milliseconds)",
-        xaxis_title="Time work ring (milliseconds)"
-    )
-
-  iplot(fig, show_link=True)
-
-def throughputRing(df1, df2, numThreads, min_time, max_time):
-  df1 = df1.loc[(min_time <= df1['timePrecessingRing']) & (df1['timePrecessingRing'] < max_time)]
-  df2 = df2.loc[(min_time <= df2['timePrecessingRing']) & (df2['timePrecessingRing'] < max_time)]
-
-  df1 = df1.loc[df_1['numberThread'] == numThreads]
-  df2 = df2.loc[df_2['numberThread'] == numThreads]
-
-  df1 = df1.sort_values(by='timePrecessingRing')
-  df2 = df2.sort_values(by='timePrecessingRing')
-
-  name1= 'Threads ' + str(numThreads) + ' before opt'
-  trace1 = go.Scatter(
-      x=df1.timePrecessingRing,
-      y=df1.throughputChain,
-      name=name1
-  )
-  name2= 'Threads ' + str(numThreads) + ' after opt'
-  trace2 = go.Scatter(
-      x=df2.timePrecessingRing,
-      y=df2.throughputChain,
-      name=name2
-  )
-
-
-  fig = go.Figure(data=[trace1, trace2])
-
-  fig.update_layout(
-      title="Throughput Chain by time processing ring",
-      yaxis_title="Number accepted messanges by one sec",
-      xaxis_title="Time work ring (milliseconds)"
-  )
-
-  iplot(fig, show_link=False)
-
-def latencyThreads(df1, df2, numThreads):
-  df1 = df1.loc[df1['numberThread'] == numThreads]
-  df2 = df2.loc[df2['numberThread'] == numThreads]
-
-  df1 = df1.sort_values(by='timePrecessingRing')
-  df2 = df2.sort_values(by='timePrecessingRing')
-
-  name1= 'Threads ' + str(numThreads) + ' before opt'
-  trace1 = go.Scatter(
-      x=df1.timePrecessingRing,
-      y=df1.latencyChain,
-      name=name1
-  )
-  name2= 'Threads ' + str(numThreads) + ' after opt'
-  trace2 = go.Scatter(
-      x=df2.timePrecessingRing,
-      y=df2.latencyChain,
-      name=name2
-  )
-
-
-  fig = go.Figure(data=[trace1, trace2])
-
-  fig.update_layout(
-        title="Latency Chain by time processing ring",
-        yaxis_title="Time to deliver message (milliseconds)",
-        xaxis_title="Time work ring (milliseconds)"
-    )
-
-  iplot(fig, show_link=True)
-
-def throughputThreadsShift(df_local, numThread1, num_message1):
-  df1 = df_local.loc[(df_local['numberThread'] == numThread1) & (df_local['numberMessages'] == num_message1)]
-
-  result = {}
-  dif_shift = df1['shift'].unique()
-  for shift in dif_shift:
-    df_op = df1.loc[df1['shift'] == shift]
-    result[shift] = df_op['throughputRing'].mean()
-
-  name1= 'Threads ' + str(numThread1) + ' and number of message ' + str(num_message1)
-  trace1 = go.Scatter(
-      x=list(result.keys()),
-      y=list(result.values()),
-      name=name1
-  )
-
-  fig = go.Figure(data=[trace1])
-
-  fig.update_layout(
-      title="Throughput with different shift",
-      yaxis_title="Throughput",
-      xaxis_title="Shift"
-  )
-
-  iplot(fig, show_link=False)
-
 def throughputOne(df):
   name1= 'Threads 6 with shift 5'
   trace1 = go.Scatter(
@@ -195,6 +16,159 @@ def throughputOne(df):
       title="Throughput Chain by epoch time",
       yaxis_title="Throughput by sec",
       xaxis_title="number of epoch"
+  )
+
+  iplot(fig, show_link=False)
+
+def throughputRing(df1):
+
+  result = {}
+  dif_threads = df1['numberThread'].unique()
+
+  for thread in dif_threads:
+    df_op = df1.loc[df1['numberThread'] == thread]
+    result[thread] = df_op['throughputRing'].mean()
+
+  trace1 = go.Scatter(
+      x=list(result.keys()),
+      y=list(result.values()),
+  )
+
+
+  fig = go.Figure(data=[trace1])
+
+  fig.update_layout(
+      title="Dependency throughput on length ring",
+      yaxis_title="Throughput",
+      xaxis_title="Threads"
+  )
+
+  iplot(fig, show_link=False)
+
+def throughputChain(df1):
+
+  result = {}
+  dif_threads = df1['numberThread'].unique()
+
+  for thread in dif_threads:
+    df_op = df1.loc[df1['numberThread'] == thread]
+    result[thread] = df_op['throughputRing'].mean() / (thread - 1)
+
+  trace1 = go.Scatter(
+      x=list(result.keys()),
+      y=list(result.values()),
+  )
+
+
+  fig = go.Figure(data=[trace1])
+
+  fig.update_layout(
+      title="Dependency throughput on length ring",
+      yaxis_title="Throughput",
+      xaxis_title="Threads"
+  )
+
+  iplot(fig, show_link=False)
+
+def latencyRing(df1):
+
+  result = {}
+  dif_threads = df1['numberThread'].unique()
+
+  for thread in dif_threads:
+    df_op = df1.loc[df1['numberThread'] == thread]
+    result[thread] = df_op['latencyChain'].mean()
+
+  trace1 = go.Scatter(
+      x=list(result.keys()),
+      y=list(result.values()),
+  )
+
+  fig = go.Figure(data=[trace1], layout={'title':'Latency Chain by time processing ring'})
+
+  fig.update_layout(
+        title="Dependency latency on number threads and shift",
+        yaxis_title="Latency",
+        xaxis_title="Threads(*)"
+    )
+
+  iplot(fig, show_link=True)
+
+def calculateDependencyLatencyRing(df1, thread_min, thread_max):
+  latency = {}
+  koefs = {}
+  dif_threads = df1['numberThread'].unique()
+
+  for thread in dif_threads:
+    df_op = df1.loc[df1['numberThread'] == thread]
+    latency[thread] = df_op['latencyChain'].mean()
+
+  for thread in dif_threads:
+    if ((thread < thread_min) or (thread > thread_max)):
+      continue
+    koefs[thread] = (latency[thread]/(thread - 1))
+
+  trace1 = go.Scatter(
+      x=list(koefs.keys()),
+      y=list(koefs.values()),
+  )
+
+  fig = go.Figure(data=[trace1])
+
+  fig.update_layout(
+        title="Dependency latency koef",
+        yaxis_title="koef",
+        xaxis_title="Threads(*)"
+    )
+  iplot(fig, show_link=True)
+  print("Avarage: " + str(sum(koefs.values()) / len (koefs.values())))
+
+def throughputRingDependencyShift(df1):
+
+  result = {}
+  dif_shift = df1['shift'].unique()
+
+  for shift in dif_shift:
+    df_op = df1.loc[df1['shift'] == shift]
+    result[shift] = df_op['throughputRing'].mean()
+
+  trace1 = go.Scatter(
+      x=list(result.keys()),
+      y=list(result.values()),
+  )
+
+
+  fig = go.Figure(data=[trace1])
+
+  fig.update_layout(
+      title="Dependency throughput on length ring",
+      yaxis_title="Throughput",
+      xaxis_title="Shift"
+  )
+
+  iplot(fig, show_link=False)
+
+def latencyRingDependencyShift(df1):
+
+  result = {}
+  dif_shift = df1['shift'].unique()
+
+  for shift in dif_shift:
+    df_op = df1.loc[df1['shift'] == shift]
+    result[shift] = df_op['latencyChain'].mean()
+
+  trace1 = go.Scatter(
+      x=list(result.keys()),
+      y=list(result.values()),
+  )
+
+
+  fig = go.Figure(data=[trace1])
+
+  fig.update_layout(
+      title="Dependency throughput on length ring",
+      yaxis_title="Latency",
+      xaxis_title="Shift"
   )
 
   iplot(fig, show_link=False)
